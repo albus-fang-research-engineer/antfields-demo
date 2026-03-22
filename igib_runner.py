0621008eb35c437432e95ae7b3f48884b7160374
Albus Fang <albus.fang.research.engineer@gmail.com>
2026-03-22 09:18:29 -0400

    perturbing lidar readings

--- igib_runner.py
+++ igib_runner.py
@@ -156,7 +156,15 @@ def get_current_lidar_frame(renderer, camera_position, hidden_instance=[]) -> np
         # print("lidar reading shape 2",lidar_readings.shape)
         lidar_readings = lidar_readings[dist > 0]
 
-        
+        dist = dist[dist > 0]
+        dist_m = dist
+        # compute sigma in meters
+        sigma_m = np.where(dist_m < 1.0, 0.01, 0.01 * dist_m)
+        # convert sigma back to mesh units
+        sigma = sigma_m
+        dirs = lidar_readings / dist[:, None]
+        range_noise = np.random.randn(len(dist)) * sigma
+        lidar_readings = lidar_readings + dirs * range_noise[:, None]
 
 
         lidar_readings[:, 2] = -lidar_readings[:, 2]  # make z pointing out

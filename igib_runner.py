3114960615c070b33aabdc380d3b50ebec662b1e
Albus Fang <albus.fang.research.engineer@gmail.com>
2026-03-26 18:08:02 -0400

    increasing sample amount back to 5000

--- igib_runner.py
+++ igib_runner.py
@@ -235,7 +235,7 @@ def sample_points_and_speeds_from_pos_new(model, position, minimum, maximum, num
     # print(bounds)
     #points, speeds, bounds = sample_points_and_speeds_from_bounds(pc, bounds, minimum=minimum, maximum=maximum, num=num)
     
-    return points[0:4000], speeds[0:4000], bounds[0:4000], surface_points[:4000]
+    return points[0:5000], speeds[0:5000], bounds[0:5000], surface_points[:5000]
 
 def sample_points_from_pos(model, position, scale_factor=1):
     """

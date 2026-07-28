d7c390a5a11b35fabe6317d4ef001f7c4924a2c2
Albus Fang <albus.fang.research.engineer@gmail.com>
2026-07-28 10:12:52 -0400

    main.py minor changes

--- igib_runner.py
+++ igib_runner.py
@@ -80,7 +80,7 @@ def batched_min_mean_distance(model, query_pts, local_obs):
 
     return min_mu
 
-def batched_cvar_distance(model, query_pts, local_obs, alpha=0.5, tail=0.1):
+def batched_cvar_distance(model, query_pts, local_obs, alpha=0.45, tail=0.1):
 
     device = model.dist_device
 
